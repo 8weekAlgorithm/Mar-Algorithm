1d0fda7c8d25938791613031e328d590006fe10d
askl690 <askl690@gmail.com>
2025-03-27 03:27:01 +0900

    B13305 내용 수정

--- WEEK 8/Yeol/B13305.java
+++ WEEK 8/Yeol/B13305.java
@@ -3,33 +3,41 @@
 // 시간: 332ms
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class B2579 {
+public class B13305 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(br.readLine()); // 계단의 수
+		int N = Integer.parseInt(br.readLine()); //도시의 개수
 		
-		int[] steps = new int[N+1];
+		long[] dist = new long[N-1]; //두 도시를 연결하는 도로의 길이 저장할 배열
 		
-		for(int i = 1; i <= N; i++) {
-			steps[i] = Integer.parseInt(br.readLine());
-		}//계단 별 점수 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N-1; i++) {
+			dist[i] = Long.parseLong(st.nextToken());
+		}//배열에 값 입력
 		
-		// DP 배열: dp[i]는 i번째 계단까지 올라왔을 때의 최대 점수
-		int[] dp = new int[N+1];
+		long[] costs = new long[N]; //각 도시의 기름 리터당 가격 저장할 배열
 		
-		if(N >= 1) dp[1] = steps[1];
-		if(N >= 2) dp[2] = steps[1] + steps[2];
-		if(N >= 3) dp[3] = Math.max(steps[1] + steps[3], steps[2] + steps[3]);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			costs[i] = Long.parseLong(st.nextToken());
+		}//배열에 값 입력
 		
-		for(int i = 4; i <= N; i++) {
-			dp[i] = Math.max(dp[i-2] + steps[i], dp[i-3] + steps[i-1] + steps[i]); // (i-2)에서 점프, (i-3) → (i-1) → i 최댓값 선택
+		
+		long totalCost = 0;//총 비용
+		long minPrice = costs[0]; //최소 가격
+		
+		for(int i = 0; i < N-1; i++) {
+			if(costs[i] < minPrice) {
+				minPrice = costs[i];
+			}
+			totalCost += minPrice * dist[i];
 		}
 		
-		System.out.println(dp[N]);
+		System.out.println(totalCost);
 	}
 }
